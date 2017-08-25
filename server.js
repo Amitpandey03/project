83faf1580cc5cffa33c3dcf37a7c909e7381487d
Amit Pandey <amitpandey.cs03@gmail.com>
2017-08-25 13:31:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end("<!DOCTYPE html><html><head>Amit</head><body><h1>Hi, I am Amit Pandey's Docker Image</h1></body></html>\n");
+  res.end("<!DOCTYPE html><html><head>Amit</head><body><h1>Hi, I am Amit Pandey's Docker Image./nHow are you?</h1></body></html>\n");
 }).listen( 80 );
 
 console.log('Server running on port ' + 80);
